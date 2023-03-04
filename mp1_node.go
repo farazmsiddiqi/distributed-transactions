d6bbeaa890eec7479af199c8f5932c326dae8d78
Mahi Kolla <mkolla2@Mahis-MacBook-Pro-10.local>
2023-03-03 23:00:34 -0600

    partially implemented handleIncomingConnections, need to figure out PQ, left TODO where necessary

--- mp1_node.go
+++ mp1_node.go
@@ -7,12 +7,26 @@ import (
 	"strconv"
 	"os"
 	"net"
-)
+	"math"
+	"container/heap"
+) 
 
 // tracks account balances --> account_number:balance
 var accounts = map[string]int{}
 // tracks proposed sequence nums from other nodes for txs I sent out  --> tx:array of proposals
 var proposals = map[string][]int{} 
+// tracks received messages -- used in reliable multicast implementation to only 
+// remulticast messages we see for first time. 
+var received = map[string]bool{}
+// tracks TCP write connections by identifier --> identifier:net.Conn connection 
+var other_connections = map[string]net.Conn{}
+// tracks largest priority proposals made by self 
+var proposal_counter = 0
+// tracks largest observed agreed priorities
+var largest_agreed_priority = 0
+// tracks if messages are deliverable or not --> transaction:true for deliverable, false otherwise
+var deliverable = map[string]bool{}
+
 
 // Prints out all account balances in alphabetic order
 func printAccountBalances() {
@@ -99,7 +113,8 @@ func parseConfigFile(identifier string, config_file string) {
 	return fileLines
 }
 
-func sendTransactionData(connections []net.Conn) {
+// node_name refers to the identifier of THIS node 
+func sendTransactionData(node_name string, connections []net.Conn) {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		transaction, err := reader.ReadString('\n')
@@ -119,10 +134,8 @@ func sendTransactionData(connections []net.Conn) {
 		proposals[transaction] = {}
 		
 		// MULTICAST MESSAGE TO ALL NODES 
-		// TODO: need to add message-type encoding: like this is a REQUEST_FOR_PROPOSALS
-		// TODO: do we need message ids ???
 		for i, conn := range connections {
-			conn.Write([]byte(f.Sprintf("%s %s\r\n", "REQUEST_FOR_PROPOSALS", transaction)))
+			conn.Write([]byte(f.Sprintf("%s %s %s\r\n", "REQUEST_FOR_PROPOSALS", node_name, transaction)))
 		} 
 
 		// WAIT FOR ALL PROPOSED SEQUENCE NUMS TO ARRIVE
@@ -141,18 +154,87 @@ func sendTransactionData(connections []net.Conn) {
 		agreed_seq = proposed_seqs[len(proposed_seqs)-1]
 
 		// MULTICAST AGREED SEQ_NUM TO ALL NODES 
-		// TODO: need to add message-type encoding: like this is a AGREED_SEQ_FOR_PROPOSAL 
 		for i, conn := range connections {
-			conn.Write([]byte(f.Sprintf("%s %s %d\r\n", "AGREED_SEQ_FOR_PROPOSAL", transaction, agreed_seq)))
+			conn.Write([]byte(f.Sprintf("%s %s %s %d\r\n", "AGREED_SEQ_FOR_PROPOSAL", node_name, transaction, agreed_seq)))
 		}
 	}
 }
 
-func handleIncomingConnections(conn net.Conn){
+func handleIncomingConnections(conn net.Conn, connections []net.Conn){
 	// TODO: handle all types of incoming connections and their requests 
-	// REQUEST_FOR_PROPOSALS <transaction string>
-	// AGREED_SEQ_FOR_PROPOSAL <transaction string> <agreed seq number>
+	// REQUEST_FOR_PROPOSALS <sender node_name> <transaction string>
+	// AGREED_SEQ_FOR_PROPOSAL <sender node_name> <transaction string> <agreed seq number>
+	// REPLY_FOR_PROPOSAL <sender node_name> <transaction> <proposed priority> 
 	// each incoming message must be remulticast (for reliable multicast) if this is first time seeing message
+
+	for {
+		//Read incoming data into string
+		reader := bufio.NewReader(conn)
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading: ", err.Error())
+			os.Exit(1)
+		} 
+
+		// RELIABLE MULTICAST IMPLEMENTATION
+		if _, exists := received[message]; exists {
+			// we have already seen this message and processed it, disregard
+			continue 
+		}
+
+		// else, this is first time seeing message, process it and re-multicast to other processes
+
+		split := strings.Split(message, " ")
+		request_type := split[0]
+		sender_node := split[1]
+
+		if (request_type == "REQUEST_FOR_PROPOSALS") {
+			priority_proposal = math.Max(proposal_counter, largest_agreed_priority) + 1
+			// REPLY_FOR_PROPOSAL <sender node_name> <transaction> <proposed priority> 
+			other_connections[sender_node].Write([]byte(f.Sprintf("%s %s %s %d\r\n", "REPLY_FOR_PROPOSAL", node_name, transaction, priority_proposal)))
+			
+			transaction := split[2] 
+			for i := 3; i < len(split)-2; i++ {
+				transaction += split[i]
+			}
+			// TODO
+			// store message in priority queue - bro how do we do a priority queue 
+			// • ordered by priority (proposed or agreed)
+
+			// • mark message as undeliverable
+			deliverable[transaction] = false
+
+			// REMULTICAST IT
+			for i, conn := range connections {
+				conn.Write([]byte(f.Sprintf("%s\r\n", message)))
+			}  
+		} else if (request_type == "AGREED_SEQ_FOR_PROPOSAL") {
+
+			// TODO 
+			// Upon receiving agreed (final) priority for a message ‘m’
+			// • Update m’s priority to final, and accordingly reorder messages in queue.
+
+			// • mark the message m as deliverable.
+			deliverable[transaction] = true
+
+			// • deliver any deliverable messages at front of priority queue
+
+			// REMULTICAST IT
+			for i, conn := range connections {
+				conn.Write([]byte(f.Sprintf("%s\r\n", message)))
+			} 
+		} else if (request_type == "REPLY_FOR_PROPOSAL") {
+			// appends proposed priority for message to list of proposals in map 
+			proposed_priority := split[len(split)-1]
+			transaction := split[2] 
+			for i := 3; i < len(split)-2; i++ {
+				transaction += split[i]
+			}
+
+			proposals[transaction] = append(proposals[transaction], proposed_priority)
+		}
+	}
 }
 
 func main() {
@@ -198,6 +280,7 @@ func main() {
 				conn, err := net.Dial("tcp", host_name+":"+port_number)
 			}
 			connections[counter] = conn
+			other_connections[node_name] = conn 
 			counter = counter + 1
 		}
     }
@@ -211,8 +294,8 @@ func main() {
 		}
 
 		//Send new connections to handler
-		go handleIncomingConnections(conn) // handle connections/transaction messages from incoming nodes 
-		go sendTransactionData(connections) // send transaction messages to other nodes 
+		go handleIncomingConnections(conn, connections) // handle connections/transaction messages from incoming nodes 
+		go sendTransactionData(identifier, connections) // send transaction messages to other nodes 
 	}
 
 	handleTransaction("DEPOSIT wqkby 10")
